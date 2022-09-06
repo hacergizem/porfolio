35157e5f7559733a6b598494403bdcd0a1637ab2
Gizem Gök <gizemgk654@gmail.com>
2022-09-06 23:51:49 +0300

    Projects section added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,35 +9,37 @@ const sectionTitle = document.querySelector('.section-title');
 
 const texts = {
     subtitleText: {
-        tr:`Merhaba, Ben <b>Gizem.</b> 
-        <br> Yaklaşık bir yıldır <b>front-end teknolojileri</b> ile uğraşıyorum.<br> 
-        Fotoğrafçılığa, kitaplara ve filmlere ilgim var. 
-        <br> Benim hakkımda daha fazla bilgi almak için lütfen bana ulaşın.`, 
-        en: `Hi, my name is Gizem.<br> 
-        I am interested in <b>front-end technologies</b> almost a year. 
-        <br> Also I like photography, books and movies. 
-        Please contact me for more information.`
+        tr: `Merhaba, Ben <b>Gizem.</b> 
+        <br> Yaklaşık iki yıldır <b>front-end teknolojileri</b> ile uğraşıyorum.<br> 
+        Fotoğrafçılığa, kitaplara ve filmlere ilgim var. <br>
+        <b>Web ve mobil geliştirme</b> üzerine çalışıyorum<br>
+        <br> Benim hakkımda daha fazla bilgi almak için lütfen bana ulaşın.`,
+        en: `Hi, my name is <b>Gizem.</b><br>
+        I'm interested in <b>front-end technologies </b> almost two year.<br>
+        I'm working on <b> web and mobile development.</b><br>
+        Also I like photography, books and movies. <br>
+        Please contact me for more information!</p>`
     },
     sectionTitle: {
         tr: `Kullandığım Teknolojiler`,
         en: 'Technologies I Use'
     },
-    motivationText:{
+    motivationText: {
         tr: ` “Çalışmadan, yorulmadan ve üretmeden, rahat yaşamak isteyen toplumlar; evvela haysiyetlerini, sonra hürriyetlerini daha sonra da istiklal ve istikballerini kaybetmeye mahkumdurlar.”  <span class="motivation-subtitle">Mustafa Kemal ATATÜRK</span>`,
         en: ` “Societies who wants to live in comfort without working, getting tired and producing are bound to lose firstly their dignity then their freedom then their independence and future.” <span class="motivation-subtitle">Mustafa Kemal ATATÜRK</span>`
-    }    
-}; 
+    }
+};
 
 /* Change Language Functions */
 
-let changeToEnglish = function(){    
+let changeToEnglish = function () {
     languageButton.innerHTML = 'TR';
     subtitle.innerHTML = texts.subtitleText.en;
     motivation.innerHTML = texts.motivationText.en;
     sectionTitle.innerHTML = texts.sectionTitle.en;
 }
 
-let changeToTurkish = function(){
+let changeToTurkish = function () {
     languageButton.innerHTML = 'EN';
     subtitle.innerHTML = texts.subtitleText.tr;
     motivation.innerHTML = texts.motivationText.tr;
@@ -48,20 +50,20 @@ let changeToTurkish = function(){
 
 const userLang = (navigator.language || navigator.userLanguage);
 
-if(userLang == 'tr-TR'){
+if (userLang == 'tr-TR') {
     changeToTurkish();
 }
-else if(userLang == 'en-EN'){
+else if (userLang == 'en-EN') {
     changeToEnglish();
 }
 
 /* Language Button Event */
 
 languageButton.addEventListener('click', function () {
-    if(languageButton.textContent === 'TR'){
+    if (languageButton.textContent === 'TR') {
         changeToTurkish();
     }
-    else{
+    else {
         changeToEnglish();
     }
 });
